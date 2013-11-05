2522f662032bb24c888bc8c78399eea7325f6478
Haiyang (harryg) Gao <harrygao.oz@gmail.com>
2013-11-06 10:55:42 +1100

    step-1

---
+++ BDDIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Input;
+using White.Core.InputDevices;
+using White.Core.UIItems;
+
+namespace BDDIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var process = Process.Start(@"C:\projects\main\Build_Output\Debug\Huxley.Application.exe", "-f myob-ar://localhost:6962/Default/_tmp/BDDTest.myox");
+			Thread.Sleep(10000);
+			
+			var mainWindow = AutomationElement.FromHandle(process.MainWindowHandle);
+			
+			var signOnWindow = mainWindow.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.AutomationIdProperty, "UserLogin"));
+			
+			var cancelButton = signOnWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "btnCancel"));
+			
+			Mouse.Instance.Click(cancelButton.Current.BoundingRectangle.TopLeft);
+		}
+	}
+}
